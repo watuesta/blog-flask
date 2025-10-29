2995d0ec457e445e79f9daae1579c6f4fdec0ca1
Will <w.atuesta@gmail.com>
2025-10-28 20:57:25 -0500

    feat: se cambia parametro debug a false

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ SQLITE = "sqlite:///project.db"
 POSTGRESQL = "postgresql+psycopg2://postgres:Atu.911012@localhost:5432/blogposts_db"
 
 class Config:
-    DEBUG = True
-    SECRET_KEY = 'dev'
+    DEBUG = False
+    SECRET_KEY = 'dev-will'
     SQLALCHEMY_DATABASE_URI = POSTGRESQL
     CKEDITOR_PKG_TYPE = 'full'
